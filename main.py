3d04505f7ef0b40e29ce87eb5e4b1e73ee61725e
Aqw6506-afk <aqw6506@gmail.com>
2026-04-08 06:21:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 
-import threading
-import tim
+import time
 
-def run_bot():
-    while True:
-        print("bot running...")
-        
-        # ✅ 這裡放你的交易程式
-        # 例如：
-        # trade()
-        
-        time.sleep(10)
-
-@app.route("/")
-def home():
-    return "Bot is running!"
-
-if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
-    app.run(host="0.0.0.0", port=10000)
+while True:
+    print("bot running...")
+    time.sleep(10)
